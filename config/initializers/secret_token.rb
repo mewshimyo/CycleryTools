47bd72700b62acf9c62188cd357e70c8e554dfc6
Harold Schreckengost <tug@alleghenycyclery.com>
2014-02-08 13:15:11 -0500

    Initial commit
    Created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CycleryTools::Application.config.secret_key_base = 'bc4ee153f4e1a7c0dde5302aeda3acffcdc3b6f615f062d02cd70543f9403bff4b3ca73d0d2bb125a33181f30c611e61f5884c43459cf9c893e50b9f5ff250fe'
